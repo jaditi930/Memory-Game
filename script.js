aec8b0967a7b97c8115e5e638462246cfe1f5245
Aditi Jain <jaditi930@gmail.com>
2024-02-07 15:17:01 +0530

    words removing

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var random_indexes=new Array(3);
 var select_count=0;
 var selected_words=[]
 
+
 //attach event listeners to all crosses
 for(let i=0;i<12;i++)
 {
@@ -45,11 +46,14 @@ function jumble_and_guess(){
         document.getElementById(`word-${i}`).children[1].innerText=`${new_words[i]}`
 
     }
+
+    let random_choices=[0,1,2,3,4,5,6,7,8,9,10,11]
+    shuffle(random_choices)
     
     //guess words at these indexes
     for(let i=0;i<3;i++)
     {
-       random_indexes[i]= Math.floor(Math.random() * 10+1);
+       random_indexes[i]= random_choices[i]
        document.getElementById(`index-${i}`).innerText=`${random_indexes[i]}th Word`
 
     }
@@ -72,20 +76,21 @@ function jumble_and_guess(){
 
 function add_word(e,i){
 
+    let found=selected_words.indexOf(new_words[i])
+    if(found!=-1)
+    return;
+
     if(select_count === 3){
     alert("Capacity full! Remove selected words to add a new word");
     return;
     }
 
+    // add word to selected list
     selected_words.push(new_words[i])
 
     document.getElementById(`index-${selected_words.length-1}`).innerText=new_words[i]
 
-    console.log("hello")
-    console.log(e.target)
     document.getElementById(`word-${i}`).classList.add("yellow_border")
-
-    document.getElementById(`word-${i}`).setAttribute("data-selected",selected_words.length-1)
 
     document.getElementById(`cross-${i}`).classList.remove("hidden")
     document.getElementById(`cross-${i}`).classList.add("show")
@@ -94,17 +99,21 @@ function add_word(e,i){
     select_count++;
 
     //remove listener
-    e.target.removeEventListener("click",add_word)
+    // e.target.removeEventListener("click",add_word)
 }
 
 function remove_word(e,i){
+
     e.stopPropagation()
-    console.log(e.target)
-    let index=document.getElementById(`word-${i}`).getAttribute("data-selected")
+
+    // remove word from selected list
+    let word_to_delete=new_words[i]
+
+    let index=selected_words.indexOf(word_to_delete)
     console.log(index)
 
     selected_words.splice(index,1)
-    document.getElementById(`word-${i}`).setAttribute("data-selected",-1)
+
     console.log(selected_words)
 
     select_count--;
@@ -123,11 +132,8 @@ function remove_word(e,i){
     document.getElementById(`cross-${i}`).classList.add("hidden")
     document.getElementById(`cross-${i}`).classList.remove("show")
 
-
-
     // add listener
     e.target.parentElement.addEventListener("click",add_word)
-
 
 }
 
